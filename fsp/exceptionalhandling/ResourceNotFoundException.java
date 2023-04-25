aee8b884cc32af480af12a553f21805a03a7086b
Keerthi475 <120001904+Keerthi475@users.noreply.github.com>
2023-04-25 16:23:24 +0530

    Update ResourceNotFoundException.java

--- fsp/exceptionalhandling/ResourceNotFoundException.java
+++ fsp/exceptionalhandling/ResourceNotFoundException.java
@@ -2,6 +2,8 @@ package com.example.fsp.exceptionalhandling;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/* Resource not found exception that extends the runtime exception,
+it is used for handling the error when a requested resource is not found */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 		private static final long serialVersionUID = 1L;
